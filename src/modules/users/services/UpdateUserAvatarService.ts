8bf59c6c7078633c34986b2234ac6cd9a874c03f
Kassuelo Moreira Okaszeski <kassuelo.mo@hotmail.com>
2022-07-06 22:27:06 -0300

    Disk Storage Provider implemented

--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -5,6 +5,7 @@ import { getCustomRepository } from 'typeorm';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
 import User from '../typeorm/entities/User';
 import uploadConfig from '@config/upload';
+import DiskStorageProvider from '@shared/providers/StorageProvider/DiskStorageProvider';
 
 interface IRequest {
   userId: string;
@@ -14,20 +15,19 @@ interface IRequest {
 class UpdateUserAvatarService {
   public async execute({ userId, avatarFilename }: IRequest): Promise<User> {
     const usersRepository = getCustomRepository(UsersRepository);
+    const storageProvider = new DiskStorageProvider();
+
     const user = await usersRepository.findById(userId);
     if (!user) {
       throw new AppError('Usuário não encontrado');
     }
     if (user.avatar) {
-      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
-      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
-
-      if (userAvatarFileExists) {
-        await fs.promises.unlink(userAvatarFilePath);
-      }
+      await storageProvider.deleteFile(user.avatar);
     }
 
-    user.avatar = avatarFilename;
+    const filename = await storageProvider.saveFile(avatarFilename);
+
+    user.avatar = filename;
 
     await usersRepository.save(user);
 
